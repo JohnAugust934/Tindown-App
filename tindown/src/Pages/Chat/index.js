e607ef34d4d489bbb902691cdeb9c659ceea2377
João Augusto <49285001+JohnAugust934@users.noreply.github.com>
2020-05-01 12:51:56 -0300

    Add files via upload

---
+++ tindown/src/Pages/Chat/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet, Text, Image } from 'react-native';
+
+export default function Chat() {
+    return (
+        <View>
+
+        </View>
+    );
+}
+const styles = StyleSheet.create ({
+
+})
+
+Chat.navigationOptions = {
+
+}
